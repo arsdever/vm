44f5453cb0d56e1281bdcca0475bb66ad408f0e8
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2019-10-26 14:18:16 +0400

    Adding documentation for CPU and RAM classes

--- src/cpu_assets/cpu.h
+++ src/cpu_assets/cpu.h
@@ -51,23 +51,85 @@ namespace vm
         virtual void stop() = 0;
 
         /**
-         * @brief Check whether the CPU is in running state
+         * @brief Check whether the CPU is in running state.
          * 
-         * @return true 
-         * @return false 
+         * @return true if CPU is in running state
+         * @return false if CPU is stopped
          */
         virtual bool isRunning() const = 0;
+        
+        /**
+         * @brief Generate a single clock tick.
+         * 
+         * @details After starting the CPU this function is used to generate external clock ticks.
+         * The meaning of the return value of the function is described by the implementer. It could be
+         *  - ticks until the current instruction will be completed
+         *  - ticks already produced
+         *  - or something else.
+         * 
+         * For actual purpose please have a look at the implementers documentation.
+         * 
+         * @return int user defined
+         */
         virtual int tick() = 0;
+
+        /**
+         * @brief The RAM instance used by the CPU.
+         * 
+         * @return RAM* the ram instance
+         */
         virtual RAM* ram() const = 0;
+        
+        /**
+         * @brief Disassemble the current instruction.
+         * 
+         * @details Disassembles and returns the disassembled string of the instruction under current
+         * program counter. This is a useful function when writing debugging tools.
+         * 
+         * @return std::string the disassembled instruction
+         */
         virtual std::string disassemble() const = 0;
 
 #ifdef DEBUGGING
+        /**
+         * @brief Dump the CPU state.
+         * 
+         * @return std::string the CPU state
+         */
         virtual std::string dump() const = 0;
 #endif
 
     protected:
+        /**
+         * @brief Fetch the next instruction.
+         * 
+         * @details Read the next instruction from RAM and place into instruction register (if exists) if necessary.
+         * This is a first of the common steps in the execution of most kinds processors.
+         * 
+         * @see decode()
+         * @see execute()
+         */
         virtual void fetch() = 0;
+
+        /**
+         * @brief Decode the fetched instruction.
+         * 
+         * @details Increment the program counter depending on the instruction and determine the executor which
+         * will execute the instruction.
+         * 
+         * @see fetch()
+         * @see execute()
+         */
         virtual void decode() = 0;
+
+        /**
+         * @brief Execute the fetched instruction.
+         * 
+         * @details Simply execute the executor, which already is decoded and knows how to execute the instruction.
+         * 
+         * @see fetch()
+         * @see decode()
+         */
         virtual void execute() = 0;
     };
 }
--- src/cpu_assets/ram.h
+++ src/cpu_assets/ram.h
@@ -20,15 +20,50 @@ namespace vm
         typedef int pos_t;
 
     public:
+        /**
+         * @brief Construct a new RAM object.
+         * 
+         * @param size the RAM size
+         */
         RAM(size_t size) : __size(size), __buffer(new uint8_t[size]{ 0 }) {}
+
+        /**
+         * @brief Destroy the RAM object
+         */
         ~RAM() { delete[] __buffer; }
 
+        /**
+         * @brief Get the data reference.
+         * 
+         * @details Returns a reference to the data cell taking place at specified address.
+         * 
+         * @tparam VALUE_TYPE type of data to be accessed. uint8_t is used by default.
+         * @param pos the address to be accessed
+         * @return VALUE_TYPE& the requested data reference
+         */
         template <typename VALUE_TYPE = uint8_t>
         VALUE_TYPE& operator[] (pos_t pos) { return (VALUE_TYPE&)__buffer[pos]; }
 
+        /**
+         * @brief Get the data const reference.
+         * 
+         * @details Returns a const reference to the data cell taking place at specified address.
+         * 
+         * @tparam VALUE_TYPE type of data to be accessed. uint8_t is used by default.
+         * @param pos the address to be accessed
+         * @return VALUE_TYPE const& the requested data reference
+         */
         template <typename VALUE_TYPE = uint8_t>
         VALUE_TYPE const& operator[] (pos_t pos) const { return (VALUE_TYPE&)__buffer[pos]; }
 
+        /**
+         * @brief Set RAM content.
+         * 
+         * @details Operator simply clears and rewrites the content from begginning.
+         * 
+         * @param list the data to be written
+         * @return RAM& current object reference
+         */
         RAM& operator = (std::initializer_list<uint8_t> const& list)
         {
             delete[] __buffer;
